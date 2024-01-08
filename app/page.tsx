bbf4d686994c88b209f07213337a7a8a2e90e0b8
Earl A. Villapaz <jasonburning16@gmail.com>
2024-01-08 18:40:08 +0800

    1: Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      Hellow
-    </div>
+    <main className="flex flex-col justify-center items-center h-full bg-[linear-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#c7a882] to-[#3d749e]">
+
+    </main>
   );
 }
